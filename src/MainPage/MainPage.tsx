911bcd703c8f97a98cc9c72a458ef70fc04eb724
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-07-09 17:44:42 +0200

    feat/add_company

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -15,8 +15,24 @@ export default function MainPage() {
         { "id": 427, "name": "Tesla" },
         { "id": 901, "name": "Alibaba" }])
 
+    const [input, setInput] = useState<string>("")
+
+    const onClickAdd = (input: string) => {
+        setCompanies([...companies, {id: companies.length + 1, name: input}])
+    }
+    console.log(companies)
 
   return (
-    <div>{companies.map(c => (<CompanyItem id={c.id} name={c.name}/>))}</div>
+    <div>
+        <input placeholder="New company" value={input} onChange={(event) => setInput(event.target.value)}/>
+        <button onClick={() => 
+            {onClickAdd(input)
+            setInput("")}}>➕</button>
+            
+        {companies
+        .map(c => (
+        <CompanyItem id={c.id} name={c.name}/>
+        ))}
+        </div>
   )
 }
