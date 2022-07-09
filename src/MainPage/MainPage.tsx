bd64a194720d529efdcd54819c471e07b4803325
tikhomyrova-tetiana <tihomirova.tania@gmail.com>
2022-07-09 18:13:38 +0200

    feat/sort_company_list

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -17,6 +17,7 @@ export default function MainPage() {
 
     const [input, setInput] = useState<string>("")
     const [search, setSearch] = useState<string>("")
+    const [sort, setSort] = useState<string>("")
 
     const onClickAdd = (input: string) => {
         setCompanies([...companies, {id: companies.length + 1, name: input}])
@@ -27,15 +28,31 @@ export default function MainPage() {
         setCompanies(updatedList)
     }
 
+    const compare_name = (company1: Company, company2: Company) => {
+        return company1.name.localeCompare(company2.name)
+    }
+    const compare_name_reverse = (company1: Company, company2: Company) => {
+        return company2.name.localeCompare(company1.name)
+    }
+    const sortedCompanies = [...companies].sort(sort === "" ? compare_name_reverse : compare_name)
+
+    const sorting = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSort(event.target.value)
+              }
+
   return (
     <div>
-        <input placeholder="🔍Search" value={search} onChange={(event) => setSearch(event.target.value)}/>
+        <input placeholder="🔍  Search" value={search} onChange={(event) => setSearch(event.target.value)}/>
         <input placeholder="New company" value={input} onChange={(event) => setInput(event.target.value)}/>
         <button onClick={() => 
             {onClickAdd(input)
             setInput("")}}>➕</button>
+        <select value={sort} onChange={sorting}>
+            <option value="name"> A-Z </option>
+            <option value=""> Z-A </option>
+        </select>
             
-        {companies
+        {sortedCompanies
         .filter((company) => company.name.toLowerCase().includes(search.toLowerCase()))
         .map(company => (
         <CompanyItem id={company.id} name={company.name} onClickRemove={onClickRemove}/>
